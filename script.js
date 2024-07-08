b4b7770699b535ef91c2adb635413a6bc146e94f
Papkin2 <bartekchuchmacz@poczta.fm>
2024-07-09 00:00:55 +0200

    kolejne zmagania
    nie ma formularza
    boksy chyba są już w miare ok

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function toggleBackground(button) {
     const textBox = button.closest('.text-box');
     textBox.classList.toggle('show-text');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textBoxHeaders = document.querySelectorAll(".text-box h2");
+
+    textBoxHeaders.forEach(header => {
+        header.addEventListener("click", function() {
+            header.scrollIntoView({ behavior: "smooth", block: "start" });
+            toggleBackground(header);
+        });
+    });
+});
