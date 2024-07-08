c852a3e99abed4d219f6077298b4b683814e13a6
Papkin2 <bartekchuchmacz@poczta.fm>
2024-07-08 17:43:15 +0200

    nowe boksy

--- script.js
+++ script.js
@@ -3,23 +3,10 @@ function toggleMenu() {
     const boxes = document.querySelectorAll('.box');
 
     menu.classList.toggle('show');
-
-    // Ukrycie otwartych boxów po kliknięciu na ikonę rozwijania menu
-    boxes.forEach(box => {
-        box.classList.remove('show-content');
-    });
 }
 
-function toggleBox(element) {
-    const boxes = document.querySelectorAll('.box');
 
-    // Hide other boxes
-    boxes.forEach(box => {
-        if (box !== element) {
-            box.classList.remove('show-content');
-        }
-    });
-
-    // Toggle the visibility of the current box
-    element.classList.toggle('show-content');
+function toggleBackground(button) {
+    const textBox = button.closest('.text-box');
+    textBox.classList.toggle('show-text');
 }
